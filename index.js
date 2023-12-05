54c1ac7e56eb2c4f898305651eedbb96adc00da1
victor <victor.odhi@gmail.com>
2023-12-05 09:47:49 +0300

    completed test

---
+++ index.js
@@ -0,0 +1,34 @@
+// defining add
+function add(a, b) {
+    return a + b;
+}
+// defining subtraction
+function subtract(a, b) {
+    return a - b;
+}
+
+// defining multiply
+function multiply(a, b) {
+    return a * b;
+}
+
+// defining divide
+function divide(a, b) {
+    return a / b;   
+}
+   
+function increment(n) {
+    return n + 1;
+}
+   
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+   
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
